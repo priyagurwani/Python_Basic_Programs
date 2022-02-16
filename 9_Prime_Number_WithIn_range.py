f59db08a0661db175b6eb06cd5a50035a014d1fb
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-02-16 18:56:43 +0530

    Prime Numbers within a Particular range:)

---
+++ 9_Prime_Number_WithIn_range.py
@@ -0,0 +1,16 @@
+a=int(input("Enter number: "))
+primes=[]
+for i in range(2,a+1):
+    prime=False
+    if(a<0):
+        continue
+    if(a==2):
+        primes.append(2)
+        continue
+    for num in range(2,i):
+        if(i%num==0):
+            prime=True
+            break
+    if prime==False:
+        primes.append(i)   #append=To add
+print(primes)
