9d581560f1646755a1677f8ac2672c8a4a78cd7f
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-02-20 18:52:47 +0530

    Updated:)

--- 8_Check_Prime_Number.py
+++ 8_Check_Prime_Number.py
@@ -9,4 +9,19 @@ if(prime==True):
     print(f"{a} is Not a Prime Number")        
 else:
     print(f"{a} is a Prime Number")
+    
+  
+# OR
+def prime(num):
+        factors=[i for i in range(2,num) if num%i==0]
+        if len(factors)==0:
+            return True
+        else:
+            return False
+    
+print(prime(10))
+print(prime(9))
+print(prime(5))
+print(prime(3))
+
 
