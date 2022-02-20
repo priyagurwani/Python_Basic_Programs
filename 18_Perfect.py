2cdaea553955c2c470623faa243b3b837862f2b4
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-02-20 17:35:22 +0530

    Tp check Perfect Number:)

---
+++ 18_Perfect.py
@@ -0,0 +1,31 @@
+# Perfect Number whose addition of factors are equal as number:
+# Eg: 6=[1,2,3] =  3+2+1 = 6 = Perfect Number
+# Eg: 10=[1,2,5] = 1+2+5 = 8 = Not Perfect Number
+# Using Iteration
+n=int(input("Enter n: "))
+sum=0
+list_perfect=[]
+for i in range(1,n):
+    if n%i==0:
+        list_perfect.append(i)
+# print(list_perfect)
+for num in list_perfect:
+    sum+=num
+# print(sum)
+if sum==n:
+    print('Its a Perfect Number', n)
+    print("And the factors are:")
+    print(list_perfect)
+else:
+    print("Its not a perfect number ", n)
+
+# OR 
+
+# using list Comprehension
+n=int(input("Enter n: "))
+sum=0
+list_perfect2=[i  for i in range(1,n) if n%i==0]
+for num in list_perfect2:
+    sum+=num
+Check=[list_perfect2 if sum==n else "Not Perfect"]
+print(Check)
