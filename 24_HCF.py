25d6c4693880d110229c079d310981c88bbb8bee
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-02-22 12:33:48 +0530

    Update 24_HCF.py

--- 24_HCF.py
+++ 24_HCF.py
@@ -25,3 +25,14 @@ while num1 != num2:
         num2 -= num1
 
 print("Hcf of", a, "and", b, "is", num1)
+
+# 3.Using for
+num1=9
+num2=18
+hcf=1
+for i in range(1, min(num1, num2)):
+
+    if num1 % i == 0 and num2 % i == 0:
+        hcf = i
+
+print("Hcf of", num1, "and", num2, "is", hcf)
